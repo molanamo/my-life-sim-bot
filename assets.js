bc338c336e4cec13af2a403fe167996e224fe6c2
molanamo <myw4930@gmail.com>
2026-06-04 23:36:48 +0330

    Create assets.js

---
+++ assets.js
@@ -0,0 +1,9 @@
+const photos = {
+  shir_khorshid: "AgACAgQAAxkBAAEqGVJqIV2gHEUpDQUUfck0hwgJx_QTjQACPxBrG4APAVGrcr1xhhY1AQEAAwIAA3gAAzsE",
+};
+
+const gifs = {
+  nuclear: "CgACAgQAAxkBAAEqEo9qIIXeZFa4e4pPIR67chxvh9D2XwACbwMAAlhtBFOyrDZljZRTyjsE",
+};
+
+module.exports = { photos, gifs };
